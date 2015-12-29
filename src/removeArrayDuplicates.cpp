40f53a69ce2802f2ddd5209d7c5f8f30282487b3
MahikanthNag <babanag95@gmail.com>
2015-12-30 02:00:16 +0530

    Submit Commit

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -10,10 +10,47 @@ ERROR CASES: Return NULL for invalid inputs.
 
 NOTES: Don't create new array, try to change the input array.
 */
-
+#include<malloc.h>
 #include <stdio.h>
 
-void * removeArrayDuplicates(int *Arr, int len)
+void* removeArrayDuplicates(int *arr, int len)
 {
+    int i,j,t,x,k=1,*arr1;arr1=(int*)malloc(100);
+    if(arr==NULL||len<0)
+        return NULL;
+
+    for(i=0;i<len;i++)
+    {
+        for(j=0;j<len-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                t=arr[j];arr[j]=arr[j+1];arr[j+1]=t;
+            }
+        }
+    }
+    x=arr[0];arr1[0]=arr[0];
+    for(i=1;i<len;i++)
+    {
+        if(arr[i]!=x)
+        {
+            arr1[k]=arr[i];x=arr[i];k++;
+        }
+    }
+    for(i=0;i<k;i++)
+    {
+        arr[i]=arr1[i];
+    }
+
 	return NULL;
 }
+/*int main()
+{
+    int i;
+    int arr[5]={1,2,3,2,1};removeArrayDuplicates(arr, 5);
+    for(i=0;i<5;i++)
+    {
+        printf("%d  ",arr[i]);
+    }
+
+}*/
